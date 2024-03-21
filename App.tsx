3266c2c2f2a9395ffa17a61d368803eb4ad9e28a
Lance Serrano <qljserrano01@gmail.com>
2024-03-22 00:00:05 +0800

    Added Recipe Page

--- App.tsx
+++ App.tsx
@@ -9,10 +9,10 @@ import Onboarding2 from './src/Onboarding2'; // Import the Onboarding2 component
 import Login from './src/Login'; // Import the Login component
 import Register from './src/Register'; // Import the Register component
 import HomeScreen from "./src/Home"; // Import the Register component
-import RecipePage from "./src/oneRecipePage"; // Import the Recipe component
 import ProfileScreen from "./src/Profile"; // Import the Profile component
 import FavoritesPage from "./src/Favorites"; // Import the Favorites component
 import MoreScreen from "./src/More"; // Import the More component
+import RecipePage from "./src/RecipePage"; // Import the Recipe component
 import { useFonts } from "expo-font";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome } from '@expo/vector-icons';
